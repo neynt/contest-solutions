65d67badf1d7ae064f704edc554ccb8cc31cfa23
Jim Zhang <Hyriodula@gmail.com>
2014-02-23 19:30:57 -0500

    fix mouse journey

--- ccc/2012-s5-mouse-journey.py
+++ ccc/2012-s5-mouse-journey.py
@@ -3,8 +3,8 @@ from collections import deque
 
 R, C = map(int, raw_input().split())
 
-dp = [[0]*C]*R
-cage = [[False]*C]*R
+dp = [[0 for _ in range(C)] for _ in range(R)]
+cage = [[False for _ in range(C)] for _ in range(R)]
 
 K = int(raw_input())
 for _ in range(K):
@@ -15,10 +15,13 @@ for r in range(R):
     for c in range(C):
         if cage[r][c]:
             dp[r][c] = 0
-        elif r == 0 or c == 0:
+        elif r == 0 and c == 0:
             dp[r][c] = 1
+        elif r == 0:
+            dp[r][c] = dp[r][c-1]
+        elif c == 0:
+            dp[r][c] = dp[r-1][c]
         else:
             dp[r][c] = dp[r-1][c] + dp[r][c-1]
 
-for row in dp:
-    print(row)
+print dp[R-1][C-1]
